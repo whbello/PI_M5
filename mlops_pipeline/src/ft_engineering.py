7391019103137f1b1cbd108b834c64aebae3f926
Wilson Bello V. <whbello@yahoo.es>
2026-02-21 13:02:11 -0500

    refactor(ml): remove commented code from ft_engineering.py

--- mlops_pipeline/src/ft_engineering.py
+++ mlops_pipeline/src/ft_engineering.py
@@ -442,16 +442,3 @@ print("="*80)
 
 
 
-
-
-
-
-#import pandas as pd
-#import numpy as np
-
-#def feature_engineering(df):
-#    """
-#    Function to perform feature engineering.
-#    """
-#    # Add your feature engineering logic here
-#    return df
